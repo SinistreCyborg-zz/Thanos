ac6dd04667e1fb711d352c6e39412d69be857ac7
SinistreCyborg <42881111+SinistreCyborg@users.noreply.github.com>
2019-06-25 18:05:02 -0400

    Set up basic bot

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "log"
+    "os"
+    "os/signal"
+    "syscall"
+    dg "github.com/bwmarrin/discordgo"
+)
+
+func main() {
+
+    // Create a new Discord session with the provided token.
+    bot, err := dg.New("Bot " + os.Getenv("token"))
+    if err != nil {
+        log.Fatal("Error creating Discord session...", err)
+        return
+    }
+
+    // Open Websocket connection.
+    err = bot.Open()
+    if err != nil {
+        log.Fatal("Error opening connection...", err)
+        return
+    }
+
+    // Wait here until Ctrl-C or some other term signal.
+    log.Print("Bot is now running. Press Ctrl-C to exit.")
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+
+    // Close connection.
+    bot.Close()
+
+}
